9f7657be6b0d8a56f38d93aeb4571c58714784d3
medeirosavio <savio.medeiros@ccc.ufcg.edu.br>
2025-05-23 06:01:04 -0300

    Finalizando etapas obrigatorias

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -1,16 +1,16 @@
-CREATE TABLE integrante (
+CREATE TABLE IF NOT EXISTS integrante (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     franquia VARCHAR(255) NOT NULL,
     funcao VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE time (
+CREATE TABLE IF NOT EXISTS time (
     id SERIAL PRIMARY KEY,
     data DATE NOT NULL
 );
 
-CREATE TABLE composicao_time (
+CREATE TABLE IF NOT EXISTS composicao_time (
     id SERIAL PRIMARY KEY,
     id_time INTEGER NOT NULL REFERENCES time(id),
     id_integrante INTEGER NOT NULL REFERENCES integrante(id)
